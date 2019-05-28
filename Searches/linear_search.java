1e5b8ad063079782d63df2703098f704c0c12666
unicorn-io <tayalvishesh83@gmail.com>
2019-05-28 18:38:56 +0530

    Searches

---
+++ Searches/linear_search.java
@@ -0,0 +1,18 @@
+public class linear_search {
+    
+    /**
+     *               Generic Linear Search Method
+     * @param array  The array to be Searched in
+     * @param item   The Item to be Seached for
+     * @return       index of the item if FOUND else -1    
+     */
+    public static <T extends Comparable<T>> int linearSearch(T[] array, int item) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == item) {
+                return i;
+            } else {
+                return -1;
+            }
+        }
+    }
+}
