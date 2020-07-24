b1e2328725158ad475d4c9ffde742c32629c7b14
unicorn-io <tayalvishesh83@gmail.com>
2020-07-24 08:03:55 +0530

    Modified DistTo approch

--- graphs/UndirectedBFS.java
+++ graphs/UndirectedBFS.java
@@ -15,18 +15,16 @@ public class UndirectedBFS {
     private void bfs(UndirectedGraph G, int s) {
         Queue<Integer> q = new LinkedList<>();
         q.offer(s);
-        int dist = 0;
-        distTo[s] = dist;
+        distTo[s] = 0;
         marked[s] = true;
         while (!q.isEmpty()) {
             int v = q.poll();
-            dist++;
             for (int w : G.adj(v)) {
                 if (!marked[w]) {
                     q.offer(w);
                     marked[w] = true;
                     edgeTo[w] = v;
-                    distTo[w] = dist;
+                    distTo[w] = distTo[v]+1;
                 }
             }
         }
